3f3e0a35ef872fd4160b54e0582d7124e8b2b5e3
LESLY VERA <leslycastillejo320@gmail.com>
2025-03-18 14:03:15 +0100

    ho aggiunto i commenti al contenuto

--- somma.py
+++ somma.py
@@ -1,7 +1,7 @@
-valore_1 = input("passami il primo valore: ")
-valore_2 = input("passami il secondo valore: ")
+valore_1 = input("passami il primo valore: ")     #richiede all'utente di inserire un numero
+valore_2 = input("passami il secondo valore: ")   #richiede all'utente di inserire un altro numero
 
-v1 = int(valore_1)
-v2 = int(valore_2)
+v1 = int(valore_1) #converte il primo valore inserito in un numero intero dall'utente
+v2 = int(valore_2) #converte il secondo valore inserito in un numero intero dall'utente
 somma = v1 + v2
-print("la somma di ", v1, "è",v2,"il risultato", somma)
+print("la somma di ", v1, "è",v2,"il risultato", somma) #stampa i numeri e la somma
