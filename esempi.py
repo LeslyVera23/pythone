8cda52c438951f087d62e73095bc94c428898862
LESLY VERA <leslycastillejo320@gmail.com>
2025-03-18 14:57:08 +0100

    ho inserito nuovo contenuto alla cartella esempi.py

--- esempi.py
+++ esempi.py
@@ -3,3 +3,8 @@
 
 #nome = "andrea ribuoli"
 #print(nome.upper() + "/n" + nome.lower())
+formato = "%3d %-26s 10%% %9.2f"
+print("Qta Prodotti IVA TOTALE\n" +
+formato % (2, "Hamburger", 2.8) + "\n" +
+formato % (1, "6 McNuggets", 4.9) + "\n" +
+formato % (1, "McMenu Large Big Mac", 8.8))
