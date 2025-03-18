85db217cd87bfa69ddc464829b9f8b11e3ba0c7e
Sawklop <124570874+LeslyVera23@users.noreply.github.com>
2025-03-18 13:50:50 +0100

    Update somma.py

--- somma.py
+++ somma.py
@@ -1,1 +1,7 @@
+valore_1 = input("passami il primo valore: ")
+valore_2 = input("passami il secondo valore: ")
 
+v1 = int(valore_1)
+v2 = int(valore_2)
+somma = v1 + v2
+print("la somma di ", v1, "è",v2,"il risultato", somma)
