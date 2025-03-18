1eb08b7d2d8fe5bb5d7adbaa6a6d1352310e9cd0
LESLY VERA <leslycastillejo320@gmail.com>
2025-03-18 16:10:44 +0100

    ho aggiunto contenuti a mesi.py

--- mesi.py
+++ mesi.py
@@ -1,1 +1,15 @@
 #scrivere un programma per tradurre i numeri da 1 a 12 nei nomi dei mesi corrispondenti in italiano
+#creazione di una stringa con tutti i mesi concatenati
+mesi = ("gennaio "  +  # 0  (1-1)*9
+        "febbraio " +  # 9  (1-1)*9
+        "marzo "    +  # 18  (1-1)*9
+        "aprile "   +  # 27  (1-1)*9
+        "maggio "   +  # 36  (1-1)*9
+        "giugno "   +  # 45  (1-1)*9
+        "luglio "   +  # 54  (1-1)*9
+        "agosto "   +  # 63  (1-1)*9
+        "settembre "+  # 72  (1-1)*9
+        "ottobre "  +  # 81  (1-1)*9
+        "novembre " +  # 90  (1-1)*9
+        "diciembre ")  # 99  (1-1)*9
+
