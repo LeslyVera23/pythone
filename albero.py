7a4b554f6d811725051e918d3f9a0ca7ce199625
LESLY VERA <leslycastillejo320@gmail.com>
2025-04-01 15:44:07 +0200

    ho aggiunto contenuto albero.py

---
+++ albero.py
@@ -0,0 +1,16 @@
+#ricevuti in input: un carattere da utilizzare, ampiezza di base. Rappresentare l' albero.
+stringa = input("Passami un carattere: ")
+base = int(input("Passami ampiezza della base: "))
+
+#print(stringa * base)
+#print("" * 2 + "A" * 1)
+#print("" * 2 + "A" * 3)
+#print("" * 2 + "A" * 5)
+
+char = input("carattere da ripetere: ")
+base = int(input("ampiezza della base: "))
+i = 1
+while i <= base :
+    print("" * ((base - i) // 2 ) +
+        char * (i + 1))
+    i += 2
