ff466ee3ba00f7f056b195157eb5b7087d83de16
LESLY VERA <leslycastillejo320@gmail.com>
2025-04-01 10:28:35 +0200

    ho aggiunto code alla cartella carte_da_pocker.py

---
+++ carte_da_pocker.py
@@ -0,0 +1,6 @@
+primo_bytes = b"\U00002764"  #cuore rosso
+secondo_bytes = b"\U0001F5A4"  #cuore nero
+terzo_bytes = b"\U0001F9E1"  #cuore arancione
+quarto_bytes = b"\U0001F49B"  #cuore giallo
+
+print(primo_bytes, secondo_bytes, terzo_bytes, quarto_bytes)
