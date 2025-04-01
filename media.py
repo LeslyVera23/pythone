4f63ad05236303284acf7b849600f23af6db2b41
LESLY VERA <leslycastillejo320@gmail.com>
2025-04-01 16:11:59 +0200

    ho inserito i valori

---
+++ media.py
@@ -0,0 +1,10 @@
+somma = 0
+numeri_letti = 0
+valore_stringa = input("Passami il prossimo valore oppure 'exit': ")
+while valore_stringa != "exit":
+    numeri_letti += 1
+    somma += int(valore_stringa)
+    valore_stringa = input("Passami il prossimo valore oppure 'exit': ")
+#print("Numeri letti sono : ", numeri_letti)
+#print("La somma dei numeri è : ", somma)
+print("La media dei numeri letti è: ", somma / numeri_letti)
