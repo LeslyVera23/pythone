1106cf046c10dff17288052feda532421495e9e4
LESLY VERA <leslycastillejo320@gmail.com>
2025-04-02 13:58:11 +0200

    inserimento testo compito

---
+++ saldo.py
@@ -0,0 +1,2 @@
+#scrivere una funzione che calcoli il saldo di un conto bancario dopo che siano trascorsi un dato numero di anni, a partire da un dato saldo iniziale
+#e con un dato tasso di interesse annuo, accreditando gli interessi annualmente
