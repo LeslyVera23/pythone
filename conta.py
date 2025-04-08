200e1e5c65d7769a259c63befa53a6407dbb2040
Sawklop <124570874+LeslyVera23@users.noreply.github.com>
2025-04-08 14:58:53 +0200

    Update conta.py
    ho aggiunto contenuto, ma il codice è sbagliato quindi bisogna correggere

--- conta.py
+++ conta.py
@@ -1,1 +1,20 @@
+saluto = "ci vediamo puntuali domani mattina"
+lista = list(saluto)
+insieme = set(lista)
+occorrenza = {} #chiave sara lettera, il valore il conteggio?
+#creiamo un dizionario con le lettere di saluto
 
+occorrenza = {}       #chiave sara lettera, il valore il conteggio
+for lettera in insieme:
+    occorrenza[lettera] = 1   #conta le occorrenze
+    pos = saluto.find(lettera)
+    if lettera in saluto[pos+1:]:
+    pos = saluto[pos+1:].find(lettera)
+    
+    occorrenza[lettera] += 1  #incrementa il conteggio
+    print(lettera, pos)    #stampa la lettera e la posizione
+
+
+
+    def conta(stringa):
+  return(["i", "a"], {"b", "c"}, {a: 5, d:2, e:1})
