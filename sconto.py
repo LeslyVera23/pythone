105d533f3c3636fa1af4227eb9f16b100f06dff3
Sawklop <124570874+LeslyVera23@users.noreply.github.com>
2025-04-08 12:34:22 +0200

    Update sconto.py
    Ho inserito contenuto, ma non abbiamo messo dentro while lo sconto, ma direttamente con il print.

--- sconto.py
+++ sconto.py
@@ -1,1 +1,33 @@
+spesa = []
+animale = []
+totSpesa = 0
+totAnimali = 0
+totSpesaSenzaAnimali = 0  
+
+articolo = int(input("Inserisci il prezzo di un articolo (inserisci -1 per terminare): "))
+tipologia = input("Inserisci la tipologia dell'articolo, 'Y' per animale 'N' per altro: ")
+
+while articolo != -1:
+    spesa.append(articolo)
+    totSpesa += articolo  
+
+    animale.append(tipologia)
+    if tipologia == 'Y':
+        totAnimali += 1
+    else:
+        totSpesaSenzaAnimali += articolo  
+
+    articolo = int(input("Inserisci il prezzo di un altro articolo (inserisci -1 per terminare): "))
+    if articolo == -1:
+        break
+    tipologia = input("Inserisci la tipologia dell'articolo, 'Y' per animale 'N' per altro: ")
+
+
+print("Lista degli articoli acquistati:", spesa)
+print("Totale spesa:", totSpesa)
+print("Totale articoli per animali:", totAnimali)
+print("Totale spesa senza gli animali:", totSpesaSenzaAnimali)
+print("sconto dei prodotti esclusi animali: ", totSpesaSenzaAnimali * 0.2)
+print("totale spesa con sconto applicato: ", totSpesa - (totSpesaSenzaAnimali * 0.2))
+
 
